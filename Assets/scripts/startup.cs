ea200dfe2b9a9e49a8fe4832c17fdfbc7df4d6f9
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-07-14 00:30:04 -0500

    Implement startup script to build collection of blocks

---
+++ Assets/scripts/startup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startup : MonoBehaviour {
+	public float startX;
+	public float endX;
+	public float startY;
+	public float endY;
+	public GameObject block;
+	// Use this for initialization
+	void Start () {
+		for (float i = startX; i < endX; i = i + 2.5F) {
+			for (float j = startY; j < endY; j = j + 1.5F){
+				Instantiate(block, new Vector3(i, 0.7F, j), Quaternion.identity);
+			}	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
